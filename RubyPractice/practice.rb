a2caf54e8746b750a84fb960e66fdc1adb8c6f7a
vsabado <37255502+vsabado@users.noreply.github.com>
2018-12-16 18:36:55 -0600

    Working with ifs, arrays, and range

--- RubyPractice/practice.rb
+++ RubyPractice/practice.rb
@@ -10,9 +10,37 @@ class MyClass
 
   def askFirstName
     puts "What is your first name?"
-    firstName = gets.chomp()
+    firstName = gets.chomp
     puts "Your first name is #{firstName}"
   end
+
+  def testIf(condition)
+    if condition
+      puts "execute"
+      puts "multiline"
+    else
+      puts "execute one line"
+    end
+  end
+
+  def understandingMutation(array)
+    p array
+    p array.reverse
+    p array
+    array.reverse!
+    p array
+  end
+
+  def understandingRange()
+    x = 1..100
+    p x
+    y = x.to_a
+    p y
+    p y.reverse
+    p y.shuffle
+    p (1..10).to_a.reverse.shuffle
+  end
+
 
 end
 
@@ -24,6 +52,15 @@ c = MyClass.new
 #Usage of escape
 #c.understandingEscape
 
-#Testing User Input
-c.askFirstName
+#User Input
+#c.askFirstName
 
+#if statements
+# c.testIf(!true)
+
+#Working with arrays and mutation
+# array = [1,2,3,4,5,6,7,8,9]
+# c.understandingMutation(array)
+
+#Working with range and array methods
+# c.understandingRange
